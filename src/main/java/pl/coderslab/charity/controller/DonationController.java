c15ec69754790537122260571f78424d43714528
MiroslawNowak <mnowak0995@gmail.com>
2024-05-22 17:19:33 +0200

    donation controller

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,10 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/form")
+public class DonationController {
+    
+}
